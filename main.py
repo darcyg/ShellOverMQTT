a3848a18551238c02892d26101b620b815dca9a0
Luuke <lukasz.bartosz1990@gmail.com>
2016-01-14 15:03:14 +0100

    Connecting to MQTT broker using configuration in file

--- main.py
+++ main.py
@@ -1,15 +1,49 @@
 import socket
 from configuration import Configuration
+import mqtt.client as mqtt
+
+Hostname = socket.gethostname()
+
+
+class MQTTClass:
+    def __init__(self, clientId=None, config=None):
+        self._mqttc = mqtt.Client(clientId)
+        self._config = config
+        self._mqttc.on_connect = self.OnConnectCallback
+        self._mqttc.on_message = self.OnMessageCallback
+        self._mqttc.on_subscribe = self.OnSubscribeCallback
+        self._mqttc.on_log = self.OnLog
+
+    def OnConnectCallback(self, mqttc, obj, flags, rc):
+        print("Connected to broker")
+
+    def OnMessageCallback(self, mqttc, obj, msg):
+        print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+    def OnSubscribeCallback(self, mqttc, obj, mid, grantedQoS):
+        print("Subscribed: " + str(mid) + " " + str(grantedQoS))
+
+    def OnLog(self, mqttc, obj, level, string):
+        print(str(level) + ": " + string)
+
+    def run(self):
+        self._mqttc.connect(self._config.Address, self._config.Port, 60)
+        self._mqttc.subscribe(self._mqttc._client_id, 0)
+
+        rc = 0
+        while rc == 0:
+            rc = self._mqttc.loop( )
+        return rc
+
 
 def main():
-    Hostname = socket.gethostname()
-    print(Hostname)
     configuration = Configuration(".config")
-    brokerCfg = configuration.GetBrokerConfiguration()
-    print(brokerCfg.Address)
-    print(brokerCfg.Port)
-    print(brokerCfg.Login)
-    print(brokerCfg.Password)
+    brokerConfig = configuration.GetBrokerConfiguration()
+    clientConfig = configuration.GetClientConfiguration()
+
+    mqttc = MQTTClass(clientConfig.ClientId, brokerConfig)
+    rc = mqttc.run()
+    print("rc: "+str(rc))
 
     return 0
 
