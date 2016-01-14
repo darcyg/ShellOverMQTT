9caa8d6a695b1e181c69bb9f952b72105df7d8d6
Luuke <lukasz.bartosz1990@gmail.com>
2016-01-14 13:55:18 +0100

    Checking configuration for missing elements

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-from configparser import ConfigParser
+from configparser import ConfigParser, NoSectionError, NoOptionError
 
 class BrokerConfiguration:
     def __init__(self, address=None, port=1883, login=None, password=None):
@@ -8,10 +8,25 @@ class BrokerConfiguration:
         self.Password = password
 
 class Configuration:
+    required = dict(
+            MQTTBroker=["Address", "Port", "Login", "Password"],
+            MQTTClient=["Name"]
+        )
+
     def __init__(self, configFile=None):
         self.filename = configFile
         self.parser = ConfigParser()
+        self.parser.read(self.filename)
+        self._checkConfigurationFile()
 
+    def _checkConfigurationFile(self):
+        for section in self.required.keys():
+            if not self.parser.has_section(section):
+                raise NoSectionError(section)
+
+            for option in self.required.get(section):
+                if not self.parser.has_option(section, option):
+                    raise NoOptionError(option, section)
 
     def GetBrokerConfiguration(self):
         self.parser.read(self.filename)
