6173ff2516dd833fc16b9f903a31725de8b8e341
Luuke <lukasz.bartosz1990@gmail.com>
2016-01-19 14:13:16 +0100

    No need for this anymore

--- main.py
+++ main.py
@@ -8,36 +8,6 @@ import os
 
 Hostname = socket.gethostname()
 
-class MQTTClass:
-    def __init__(self, clientId=None, config=None):
-        self._mqttc = mqtt.Client(clientId)
-        self._config = config
-        self._mqttc.on_connect = self.OnConnectCallback
-        self._mqttc.on_message = self.OnMessageCallback
-        self._mqttc.on_subscribe = self.OnSubscribeCallback
-        self._mqttc.on_log = self.OnLog
-
-    def OnConnectCallback(self, mqttc, obj, flags, rc):
-        print("Connected to broker")
-
-    def OnMessageCallback(self, mqttc, obj, msg):
-        print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
-
-    def OnSubscribeCallback(self, mqttc, obj, mid, grantedQoS):
-        print("Subscribed: " + str(mid) + " " + str(grantedQoS))
-
-    def OnLog(self, mqttc, obj, level, string):
-        print(str(level) + ": " + string)
-
-    def run(self):
-        self._mqttc.connect(self._config.Address, self._config.Port, 60)
-        self._mqttc.subscribe(self._mqttc._client_id, 0)
-
-        rc = 0
-        while rc == 0:
-            rc = self._mqttc.loop( )
-        return rc
-
 
 def SetupLogging():
     path = "logging.json"
@@ -55,16 +25,13 @@ def SetupLogging():
     else:
         raise FileNotFoundError("Logging configuration")
 
+
 def main():
     SetupLogging()
-    logger = logging.getLogger("logger")
     configuration = Configuration(".config")
     brokerConfig = configuration.GetBrokerConfiguration()
     clientConfig = configuration.GetClientConfiguration()
 
-    mqttc = MQTTClass(clientConfig.ClientId, brokerConfig)
-    rc = mqttc.run()
-    print("rc: "+str(rc))
 
     return 0
 
